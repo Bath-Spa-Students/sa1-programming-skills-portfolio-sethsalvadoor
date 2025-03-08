e02d8a01e429f6502ad72005fdf858b571da9ed4
sethsalvadoor <sethsalvador@gmail.com>
2025-03-08 12:08:57 +0400

    simple sums

---
+++ Exercise02.py
@@ -0,0 +1,9 @@
+#Declare a variable and initialize it with the integer value
++ num1=8 
+#Declare a second variable and initialize
++ num2=10
+#Declare a third variable that stores the sum of first two numbers.
++ sum_result=num1+num2 
+#Print the value of the sum to the console.
++ print(sum_result) 
+
